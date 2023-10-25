7ef9391dda2de69035a4479bc044c037585b6701
dagopla <dagopla@gmail.com>
2023-10-25 15:01:39 -0500

    envio de correos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,11 +16,16 @@ module.exports = {
 						'0%': { width: '100%' },
 						'30%': { width: '60%' },
 						'100%': { width: '0%' },
-					}
+					},
+					'show': {
+						'0%': { opacity: '0', scale: '0' },
+						'100%': { opacity: '1', scale: '1' },
+					},
 				},
 				animation: {
 					'linearwidth': 'linaerwidth 1s linear ',
-					'linear-width': 'linear-width 1s linear '
+					'linear-width': 'linear-width 1s linear ',
+					'show': 'show 1s ease-in-out',
 				},
 
 		},
